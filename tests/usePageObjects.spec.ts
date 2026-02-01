9107d4b44a5b9e3147e822541be05635bbd3bcf4
Lamija <lamija@MacBook-Pro-3.local>
2026-02-01 18:24:08 +0100

    Additional commit

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,15 +1,30 @@
 import{test, expect} from '@playwright/test'
-import{NavigationPage} from '../page-objects/navigationPage'
+import{PageManager} from '../page-objects/pageManager'
+
 
 test.beforeEach(async ({page})=> {
     await page.goto('http://localhost:4200/')
 })
 
 test('navigate to form page', async({page})=> {
-    const navigateTo = new NavigationPage(page)
-    await navigateTo.formLayoutsPage()
-    await navigateTo.datePickerPage()
-    await navigateTo.smartTablePage()
-    await navigateTo.toastrPage()
-    await navigateTo.tooltipPage()
+    const pm = new PageManager(page)
+    
+    await pm.navigateTo().formLayoutsPage()
+    await pm.navigateTo().datePickerPage()
+    await pm.navigateTo().smartTablePage()
+    await pm.navigateTo().toastrPage()
+    await pm.navigateTo().tooltipPage()
+})
+
+test('parametrized methods', async({page})=> {
+    const pm = new PageManager(page)
+    
+
+    await pm.navigateTo().formLayoutsPage()
+    await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox('John Smith', 'Zohn@test.com', true)
+    await pm.navigateTo().datePickerPage()
+    await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(5)
+    await pm.onDatePickerPage().selectDatePickerWithRangeFromToday(6, 15)
+
 })
